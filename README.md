c6fe847d83a8bc2c1af3d5d0a51ec348c4742fea
Aleksander Bies <aleksanderbies@gmail.com>
2021-05-14 11:30:43 +0200

    weather for capital city if user not allow for location

---
+++ README.md
@@ -0,0 +1,29 @@
+# iWeather web app
+
+Web Weather app with Firebase connection
+
+## Technologies
+
+* JavaScript / JQuerry / AJAX
+* HTML 5
+* CSS 3
+* Google Firebase
+
+## Usage
+Web app that shows weather in current location (if user agree for geolocation, otherwise app shows weather for the capital of the country in which the user is located).
+User can also sign up to see weather and 8-days forecast in all cities from the world. User also can add five different cities to his favorites cities list and see the weather for them everytime he wants.
+
+License
+----
+
+MIT
+
+## Status of project: 
+```diff 
+- PROCESS
+```
+
+## See also
+
+* [iWeather app made in Flutter for Android and iOS mobile devices done by my colleague - Adam Chylaszek] (https://github.com/krygo-dev/iWeatherApp)
+* [iWeather desktop version made in C# for Windows done by my collegue - Adam Barczyk] (https://github.com/AdamBarczyk/iWeatherApp-)
--- assets/js/locate.js
+++ assets/js/locate.js
@@ -9,12 +9,49 @@ const humidityValue = document.getElementById("humidityValue");
 const windValue = document.getElementById("windValue");
 const description = document.getElementById("descriptionW");
 
-var latitude, longitude;
+var latitude;
+var longitude;
 
 var APIkey = 'f6785dbf88aab1a3ecaba3da1633ee5f';
 
+let country_code = geoplugin_countryCode();
+
+console.log(country_code);
+fetch(`https://restcountries.eu/rest/v2/alpha/${country_code}`)
+    .then(response => response.json())
+    .then(dataCountry =>{
+        var capital = dataCountry['capital'];
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&appid=${APIkey}`)
+        .then(response => response.json())
+        .then(data => {
+            var nameValue = data['name'];
+            var tempValue = data['main']['temp'];
+            var iconCode = data['weather'][0]['icon'];
+            var weatherDescription = data['weather'][0]['description'];
+            var humidity = data['main']['humidity'];
+            var pressure = data['main']['pressure']
+            var feels = data['main']['feels_like'];
+            var tempMax = data['main']['temp_max'];
+            var tempMin = data['main']['temp_min'];
+            var wind = data['wind']['speed'];
+                
+            locationCity.innerHTML = nameValue;
+            iconBox.innerHTML = `<img src="http://openweathermap.org/img/wn/${iconCode}@2x.png" width="120px" />`;
+            temperatureValue.innerHTML = parseInt(tempValue) + ` °<span>C</span>`;
+            minTemp.innerHTML = parseInt(tempMin) + ` °<span>C</span>`;
+            maxTemp.innerHTML = parseInt(tempMax) + ` °<span>C</span>`;
+            feelTemp.innerHTML = parseInt(feels) + ` °<span>C</span>`;
+            pressureValue.innerHTML = parseInt(pressure) + `<span> hPa</span>`;
+            humidityValue.innerHTML = parseInt(humidity) + `<span> %</span>`;
+            windValue.innerHTML = parseInt(wind) + `<span> km/h</span>`;
+            description.innerHTML = weatherDescription;
+        });
+    });
+
+
+
+
 navigator.geolocation.getCurrentPosition(function (position){
-    console.log(position);
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
 
