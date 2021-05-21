62ae1194a74a1a6497fb564bd1ee8fa78817f679
Aleksander Bies <aleksanderbies@gmail.com>
2021-05-21 12:35:54 +0200

    add city fixed

--- assets/js/db.js
+++ assets/js/db.js
@@ -22,21 +22,38 @@ function deleteFromDB(item){
         }
     });
 }
-var a = true;
+
 function addToFav(item){
     auth.onAuthStateChanged(firebaseUser => {
         if(firebaseUser){
-            
-            
             var cityCode = $(item).attr("name");
             var cityName = document.getElementById("cityName").innerHTML;
-                    
-            firebase.database().ref(`${firebaseUser.uid}`).child('favourites').child(`${cityCode}`).update({
+
+            firebase.database().ref(`${firebaseUser.uid}`).once('value', function(snapshot){
+                if (snapshot.hasChild("favourites")){
+                    firebase.database().ref(`${firebaseUser.uid}/favourites`).once('value', function(snapshot){
+                        let cities = snapshot.val();
+                        x = Object.keys(cities).length;
+                        if(x<5){
+                            firebase.database().ref(`${firebaseUser.uid}`).child('favourites').child(`${cityCode}`).update({
+                                id: cityCode,
+                                name: `${cityName}`
+                            });
+                            document.getElementById("fav").style.display="block";
+                            document.getElementById("notfav").style.display="none";
+                        }else{
+                            alert("You cannot have more than five favorites cities!");
+                        }
+                    });
+                }else{
+                    firebase.database().ref(`${firebaseUser.uid}`).child('favourites').child(`${cityCode}`).update({
                         id: cityCode,
                         name: `${cityName}`
+                    });
+                    document.getElementById("fav").style.display="block";
+                    document.getElementById("notfav").style.display="none";
+                }
             });
-            document.getElementById("fav").style.display="block";
-            document.getElementById("notfav").style.display="none";
-            }
-        });
+        }
+    });
 }
